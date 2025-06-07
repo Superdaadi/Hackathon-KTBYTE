090c577eecfb940c9b2aedcf1063b5c7aeee2fc9
Superdaadi <dadimanuelsch@gmail.com>
2025-06-08 00:45:00 +0200

    google chart

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-chart',
@@ -7,6 +9,41 @@ import { Component } from '@angular/core';
   templateUrl: './chart.component.html',
   styleUrl: './chart.component.css'
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit{
 
+  ngOnInit(): void {
+    this.loadGoogleCharts();
+  }
+
+  loadGoogleCharts(): void {
+    const script = document.createElement('script');
+    script.src = 'https://www.gstatic.com/charts/loader.js';
+    script.onload = () => this.drawChart();
+    document.head.appendChild(script);
+  }
+
+  drawChart(): void {
+    google.charts.load('current', { packages: ['corechart'] });
+    google.charts.setOnLoadCallback(() => {
+      const data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Work', 11],
+        ['Eat', 2],
+        ['Commute', 2],
+        ['Watch TV', 2]
+      ]);
+
+      const options = {
+        title: '',
+        backgroundColor: '#D2E2B0',
+        pieHole: 0.4,
+        legend: { 
+          position: 'bottom'
+        },
+      };
+
+      const chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+      chart.draw(data, options);
+    });
+  }
 }
