91031c2c0aa1fc9c6fe2c1700152230ed05b6e20
Danny-123-code <d.staller2007@gmail.com>
2025-06-07 23:15:06 +0200

    Home fertig

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ChartComponent } from './chart/chart.component';
 
 export const routes: Routes = [
     {path: '', component: HomepageComponent},
     {path:'navbar', component: NavbarComponent},
+    {path:'chart', component: ChartComponent},
+
 ];
