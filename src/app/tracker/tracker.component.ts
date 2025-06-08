5274b12b754c6ace0556a453b7d28f264e1fc33e
Superdaadi <dadimanuelsch@gmail.com>
2025-06-08 14:03:45 +0200

    Added Hover Feature

--- src/app/tracker/tracker.component.ts
+++ src/app/tracker/tracker.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, ViewChild } from '@angular/core';
+
+import { NgStyle } from '@angular/common';
 
 import { ChartComponent } from '../chart/chart.component';
 import dataDE from '../../data/task-DE.json'
@@ -8,24 +10,51 @@ import { LangService } from '../../service/lang.service';
 @Component({
   selector: 'app-tracker',
   standalone: true,
-  imports: [ChartComponent],
+  imports: [ChartComponent, NgStyle],
   templateUrl: './tracker.component.html',
   styleUrl: './tracker.component.css'
 })
 export class TrackerComponent {
 
+  protected id: number = 0
 
   protected type: number = 0
+  public mouseX: number = 0;
+  public mouseY: number = 0;
+
+  public isHovering = false;
+
+  @ViewChild('trackArea') trackArea!: ElementRef;
 
 
   constructor(public langService: LangService) {
+  }
+
+  ngOnInit() {
+
   }
 
   protected setType(type: number) {
     this.type = type
   }
 
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    if (!this.isHovering) return;
+    this.mouseX = event.clientX;
+    this.mouseY = event.clientY;
+  }
   
+  mouseEnter(id: number) {
+    this.isHovering = true;
+    this.id = id - 1
+  }
+
+  mouseLeave() {
+    this.isHovering = false;
+    console.log("leafe")
+  }
 
 
 }
