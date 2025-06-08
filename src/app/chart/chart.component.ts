0d4cd0efe947eabc2ca2e45c0362b0709604b8d7
Superdaadi <dadimanuelsch@gmail.com>
2025-06-08 14:46:35 +0200

    diagram basic

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LangService } from '../../service/lang.service';
+
 declare var google: any;
 
 @Component({
@@ -11,26 +13,59 @@ declare var google: any;
 })
 export class ChartComponent implements OnInit{
 
-  ngOnInit(): void {
+  nutrition: number = 1
+
+
+  ngOnInit() {
     this.loadGoogleCharts();
   }
 
-  loadGoogleCharts(): void {
+  constructor(public langService: LangService) {}
+
+  getDataFromLocal(key: number):number {
+    if(key = 1) {
+
+      for (let i = 0; i > 10; i++) {
+        console.log(i);
+      }
+    }
+    const valueFromLocal = localStorage.getItem(key.toString());
+
+    if(valueFromLocal)
+      return 1
+    else
+      return 0
+  }
+
+
+
+  getOneItem(key: number):boolean {
+    const valueFromLocal = localStorage.getItem(key.toString());
+    if(valueFromLocal == "true")
+      return true
+    else
+      return false
+  }
+
+
+
+  loadGoogleCharts() {
     const script = document.createElement('script');
     script.src = 'https://www.gstatic.com/charts/loader.js';
     script.onload = () => this.drawChart();
     document.head.appendChild(script);
   }
 
-  drawChart(): void {
+  drawChart() {
     google.charts.load('current', { packages: ['corechart'] });
     google.charts.setOnLoadCallback(() => {
       const data = google.visualization.arrayToDataTable([
         ['Task', 'Hours per Day'],
-        ['Work', 11],
-        ['Eat', 2],
-        ['Commute', 2],
-        ['Watch TV', 2]
+        [this.langService.tasks[0].cathegory, this.getDataFromLocal(1)],
+        [this.langService.tasks[1].cathegory, 2],
+        [this.langService.tasks[2].cathegory, 2],
+        [this.langService.tasks[3].cathegory, 2],
+        [this.langService.tasks[4].cathegory, 2]
       ]);
 
       const options = {
